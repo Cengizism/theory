268a9b7fa068edb6a7efb8fc60ff65613f76c826
Cengiz Ulusoy <ulusoy.cengiz@gmail.com>
2015-10-07 17:02:37 +0200

    Reorganize client application logic more

---
+++ client/app/app.run.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('theoryApp')
+  .run(function ($rootScope, $location, Auth) {
+    // Redirect to login if route requires auth and you're not logged in
+    $rootScope.$on('$stateChangeStart', function (event, next) {
+      Auth.isLoggedInAsync(function(loggedIn) {
+        if (next.authenticate && !loggedIn) {
+          event.preventDefault();
+          $location.path('/login');
+        }
+      });
+    });
+  });
